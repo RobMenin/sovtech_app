b52c3546f844da93c1288429a2c23508bf0c4f5b
RobMenin <95689165+RobMenin@users.noreply.github.com>
2021-12-09 09:59:15 +0200

    Update Home.js

--- app/src/Component/Home.js
+++ app/src/Component/Home.js
@@ -7,7 +7,7 @@ return(
     <>
   <div className="mainSection">
       <div className="contentBox">
-<h1>Why I would like to be join Sovtech</h1>
+<h1>Why I would like to join Sovtech</h1>
 <p>I am thrilled to see an opportunity to further my skills and knowledge. My background in front-end, back-end development and user interfaces would make me a great fit for the graduate programme at SovTech. I directly relate to a number of SovTech's spex. I have circled my top 5 most relatable Spex as seen to the right.</p>
      <div className="btnBox">
          <div className="btn">
